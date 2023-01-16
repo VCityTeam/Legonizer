88caf7e60a91f12bdd64b94eb03995458af31c26
CorentinLIRIS <corentin.chignin@hotmail.fr>
2023-01-16 09:39:02 +0100

    Enhanced legonizer

--- src/DataProcessing.js
+++ src/DataProcessing.js
@@ -6,51 +6,49 @@ const THREE = require('three');
  * @param {BufferGeometry} geometryBuffer Geometry that will be transform in heightmap
  * @param {Int32Array} ratio ratio to voxelize the modelisation
  */
-const createHeightMapFromBufferGeometry = function(geometryBuffer, ratio) {
+const createHeightMapFromBufferGeometry = function(geometryBuffer, ratio, xPlates, yPlates) {
 
-    if (!geometryBuffer.attributes.position.array) throw new Error('geometryBuffer empty');
+  if (!geometryBuffer.attributes.position.array) throw new Error('geometryBuffer empty');
 
-    if (ratio == null) throw new Error('ratio parameter not given');
-  
-    geometryBuffer.computeBoundingBox(); //Generate Bounding box of the geometry
-  
-    const maxLegoHeight = 10;
-    const bbMockUp = geometryBuffer.boundingBox;
-    const widthMockUp = bbMockUp.max.x - bbMockUp.min.x;
-    const stepDistance = widthMockUp / ratio;
-    const ratioY = Math.trunc(
-      Math.abs(bbMockUp.min.y - bbMockUp.max.y) / stepDistance
-    );
-    const mesh = new THREE.Mesh(geometryBuffer);
-    const raycaster = new THREE.Raycaster();
-    const maxZMockup = bbMockUp.max.z;
-    const heightMap = Array.from(Array(ratioY), () => new Array(ratio));
-  
-    for (let j = 0; j < ratioY; j++) {
-      for (let i = 0; i < ratio; i++) {
-        const positionRaycast = new THREE.Vector3(
-          bbMockUp.min.x + i * stepDistance,
-          bbMockUp.min.y + j * stepDistance,
-          maxZMockup
-        );
-        raycaster.set(positionRaycast, new THREE.Vector3(0, 0, -1));
-        const objects = raycaster.intersectObject(mesh);
-        if (objects.length > 0) {
-          const object = objects[0];
-          heightMap[j][i] = maxZMockup - object.distance;
-        } else {
-          heightMap[j][i] = 0;
-        }
+  if (ratio == null) throw new Error('ratio parameter not given');
+
+  geometryBuffer.computeBoundingBox(); //Generate Bounding box of the geometry
+  const ratioX = xPlates;
+  const ratioY = yPlates;
+  const maxLegoHeight = 10;
+  const bbMockUp = geometryBuffer.boundingBox;
+  const widthMockUp = bbMockUp.max.x - bbMockUp.min.x;
+  const stepDistance = widthMockUp / ratio / ratioX;
+  const mesh = new THREE.Mesh(geometryBuffer);
+  const raycaster = new THREE.Raycaster();
+  const maxZMockup = bbMockUp.max.z;
+  const heightMap = Array.from(Array(ratioY * ratio), () => new Array(ratioX * ratio));
+
+  for (let j = 0; j < ratioY * ratio; j++) {
+    for (let i = 0; i < ratioX * ratio; i++) {
+      const positionRaycast = new THREE.Vector3(
+        bbMockUp.min.x + i * stepDistance,
+        bbMockUp.min.y + j * stepDistance,
+        maxZMockup
+      );
+      raycaster.set(positionRaycast, new THREE.Vector3(0, 0, -1));
+      const objects = raycaster.intersectObject(mesh);
+      if (objects.length > 0) {
+        const object = objects[0];
+        heightMap[j][i] = maxZMockup - object.distance;
+      } else {
+        heightMap[j][i] = 0;
       }
     }
-    // Lego transformation
-    const ratioZ = maxZMockup / maxLegoHeight;
-    for (let i = 0; i < heightMap.length; i++) {
-      for (let j = 0; j < heightMap[i].length; j++)
-        heightMap[i][j] = Math.trunc(heightMap[i][j] / ratioZ);
-    }
-    return heightMap;
   }
+  // Lego transformation
+  const ratioZ = maxZMockup / maxLegoHeight;
+  for (let i = 0; i < heightMap.length; i++) {
+    for (let j = 0; j < heightMap[i].length; j++)
+      heightMap[i][j] = Math.trunc(heightMap[i][j] / ratioZ);
+  }
+  return heightMap;
+}
 
     /**
    * 
